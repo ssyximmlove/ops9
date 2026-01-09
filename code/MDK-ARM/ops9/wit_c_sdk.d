400b097fa3289a92dc0a9fae4a44eb4d26aa6e8b
Nymphaea <mail@supaku.cn>
2026-01-09 14:52:03 +0800

    [feat] 以Submodule形式管理STM32端代码

--- code/MDK-ARM/ops9/wit_c_sdk.d
+++
@@ -1,6 +0,0 @@
-ops9\wit_c_sdk.o: ..\Bsp\wit_c_sdk\wit_c_sdk.c
-ops9\wit_c_sdk.o: ..\Bsp\wit_c_sdk\wit_c_sdk.h
-ops9\wit_c_sdk.o: D:\YingYong\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
-ops9\wit_c_sdk.o: D:\YingYong\Keil5\ARM\ARMCC\Bin\..\include\stdio.h
-ops9\wit_c_sdk.o: D:\YingYong\Keil5\ARM\ARMCC\Bin\..\include\string.h
-ops9\wit_c_sdk.o: ..\Bsp\wit_c_sdk\REG.h
